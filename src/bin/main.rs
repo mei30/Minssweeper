e5508ed7a54c67a2c696733b98227abb736a1215
mei30 <meisamjafarimosleh@gmail.com>
2023-07-28 13:24:44 +0330

    Complete command-lined board module

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use minssweeper::MinsSweeper;
+
+fn main() {
+    let mut game = MinsSweeper::new(10, 10);
+
+    game.start();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,151 @@
+use rand::Rng;
+
+fn add(u: usize, i: i32) -> Option<usize> {
+    if i.is_negative() {
+        u.checked_sub(i.wrapping_abs() as u32 as usize)
+    } else {
+        u.checked_add(i as usize)
+    }
+}
+
 // Temporarily public
-
-use std::fmt::DebugStruct;
-
 #[derive(Clone)]
 enum SquareStatus {
-    BOMB,
+    MIN,
     CLEAR(u16),
 }
 
 #[derive(Clone)]
 struct BoardSquare {
-    square_status: SquareStatus,
+    status: SquareStatus,
     is_revealed: bool,
     sign: char,
 }
-pub struct Board {
+struct Board {
     grid: Vec<Vec<BoardSquare>>,
+    row: usize,
+    column: usize,
 }
 
 impl Board {
-    pub fn new(row: usize, column: usize) -> Board {
+    fn new(row: usize, column: usize) -> Board {
         let mut grid: Vec<Vec<BoardSquare>> = Vec::with_capacity(row);
 
-        for i in grid.iter_mut() {
-            *i = vec![
+        for _ in 0..grid.capacity() {
+            grid.push(vec![
                 BoardSquare {
-                    square_status: SquareStatus::CLEAR(0),
+                    status: SquareStatus::CLEAR(0),
                     is_revealed: false,
                     sign: '#'
                 };
                 column
-            ]
+            ]);
         }
 
-        Board { grid }
+        Board { grid, row, column }
     }
 
-    fn get_ith_square()
+    fn init(&mut self) {
+        self.place_mines();
+        self.count_neighboring_mines();
+        self.display();
+    }
+
+    fn get_square(&mut self, row: usize, column: usize) -> &mut BoardSquare {
+        &mut self.grid[row][column]
+    }
+
+    fn place_mines(&mut self) {
+        let mut rng = rand::thread_rng();
+
+        let mut i = 0;
+
+        // TODO: Number of mins should be configurable
+        while i < 5 {
+            i += 1;
+
+            let row = rng.gen_range(0..self.row);
+            let column = rng.gen_range(0..self.column);
+
+            let board_square = &mut self.grid[row][column];
+
+            match board_square.status {
+                SquareStatus::MIN => {
+                    i -= 1;
+                    continue;
+                }
+                SquareStatus::CLEAR(_) => {
+                    *board_square = BoardSquare {
+                        status: SquareStatus::MIN,
+                        is_revealed: false,
+                        sign: '#',
+                    }
+                }
+            }
+        }
+    }
+
+    fn count_neighboring_mines(&mut self) {
+        for i in 0..self.row {
+            for j in 0..self.column {
+                if let SquareStatus::CLEAR(_) = self.grid[i][j].status {
+                    let mut min_count = 0;
+
+                    let a: [i32; 3] = [-1, 0, 1];
+
+                    for dr in a {
+                        for dc in a {
+                            let r = add(i, dr);
+                            let c = add(j, dc);
+
+                            if let None = r {
+                                continue;
+                            }
+
+                            if let None = c {
+                                continue;
+                            }
+
+                            let r = r.unwrap();
+                            let c = c.unwrap();
+
+                            if (r < self.row) && (c < self.column) {
+                                min_count += if let SquareStatus::MIN = self.grid[r][c].status {
+                                    1
+                                } else {
+                                    0
+                                };
+                            }
+                        }
+                    }
+
+                    self.grid[i][j] = BoardSquare {
+                        status: SquareStatus::CLEAR(min_count),
+                        is_revealed: false,
+                        sign: '#',
+                    }
+                }
+            }
+        }
+    }
+
+    fn display(&self) {
+        print!("  ");
+        for i in 0..self.row {
+            print!("{} ", i);
+        }
+        print!("\n");
+        for i in 0..self.row {
+            print!("{} ", i);
+            for j in 0..self.column {
+                print!("{} ", self.grid[i][j].sign);
+            }
+            println!("\n");
+        }
+    }
 }
 
-pub struct Sweeper {
+struct Sweeper {
     board: Board,
 }
 
@@ -48,11 +154,17 @@ pub struct MinsSweeper {
 }
 
 impl MinsSweeper {
-    fn new(row: usize, column: usize) -> MinsSweeper {
+    pub fn new(row: usize, column: usize) -> MinsSweeper {
         let board = Board::new(row, column);
+
+        println!("10X10 MinsSweeper Game\n");
 
         MinsSweeper {
             sweeper: Sweeper { board },
         }
+    }
+
+    pub fn start(&mut self) {
+        self.sweeper.board.init();
     }
 }
