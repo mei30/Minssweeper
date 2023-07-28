ff48063444a8f30f8baaae6218304c47b0a2d22f
mei30 <meisamjafarimosleh@gmail.com>
2023-07-28 19:54:28 +0330

    Add future issues in source code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,9 @@
 use minssweeper::MinsSweeper;
 
+// Future Improvements
+// Add a beautiful graphical ui for game
+// Separate each part into conceptual modules
+// Apply rust patterns as much as possible
 fn main() {
     let mut game = MinsSweeper::new(10, 10);
 
